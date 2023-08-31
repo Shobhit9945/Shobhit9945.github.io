956d5c526b33cbb5eb457324b64666eb815d5412
Shobhit9945 <shobhitbaijal5@gmail.com>
2023-08-31 17:24:08 +0530

    Add files via upload

---
+++ assets/js/projects.js
@@ -0,0 +1,47 @@
+const projects = [
+    {
+        "name": "ShoBOT",
+        "description": "ShoBOT is a multipurpose discord bot. It contains many features like Moderation, Music, Games, Utilities and more",
+        "url": "https://shobot.ml/"
+    },
+    {
+        "name": "Mail OTP system",
+        "description": "An OTP generation system that sends the OTP to the user's email",
+        "url": "https://sourceb.in/PHUJ3wO2q5"
+    },
+    {
+        "name": "Guess the number",
+        "description": "Guess the number game",
+        "url": "https://sourceb.in/M2ZOr7hTXX"
+    },
+    {
+        "name": "Text Assistant app",
+        "description": "Advanced text assistant",
+        "url": "https://drive.google.com/file/d/1OIpvje2KP4k1UiIPOzsnmulD5rU8Oobv/view?usp=sharing"
+    },
+    {
+        "name":"Voice Assistant GUI app",
+        "description":"advanced voice assistant in GUI",
+        "url":"https://drive.google.com/file/d/1Y9uapWAgFjEq9QgGkSuMf9N7uafOgAoC/view?usp=sharing"
+    },
+    {
+        "name":"Voice Assistant Console app",
+        "description":"advanced voice assistant in console application",
+        "url":"https://drive.google.com/file/d/1Y7CAOwBvui2C34LYQavtJ1DmonpwoiEr/view?usp=sharing"
+    },
+    {
+        "name":"Eye Cursor",
+        "description":"Control your mouse cursor with your eyes",
+        "url":"https://sourceb.in/A4OxVcwadO"
+    },
+    {
+        "name":"Tic Tac Toe",
+        "description":"Simple Tic Tac Toe game",
+        "url":"https://sourceb.in/s4HpT9hLgg"
+    },
+    {
+        "name":"Rock Paper Scissors",
+        "description":"Simple Rock Paper Scissors game",
+        "url":"https://sourceb.in/s4HpT9hLgg"
+    }
+]
