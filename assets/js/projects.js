10ca5626286b0272a00be9bd801e2f57e323cd94
Shobhit9945 <shobhitbaijal5@gmail.com>
2026-01-28 20:08:56 +0900

    Update projects.js

--- assets/js/projects.js
+++ assets/js/projects.js
@@ -2,7 +2,7 @@ const projects = [
     {
         "name": "DocNearMe",
         "description": "DocNearMe is a Japan-focused clinic booking platform that helps patients find nearby clinics and book appointments in advance—reducing language barriers and making clinic visits faster and more convenient.",
-        "url":"https://docnearme.jp"    
+        "url":"https://docnearme.app"    
     },
     {
         "name": "Sugar Professor",
